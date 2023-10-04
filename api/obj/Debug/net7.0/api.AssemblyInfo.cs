247222da69aa75ba8972021dab5458e875bb8c5a
Marcos Silva de Souza <marcossilvadesouza.dev@gmail.com>
2023-10-04 07:42:36 -0400

    arquitetura e exemplo de uso

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b74fe607731fd7627a449d97fe61331754fe963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49c500f0e771476b99636adb4753316647589f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
